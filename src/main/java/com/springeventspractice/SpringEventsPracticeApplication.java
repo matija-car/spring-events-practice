12c53c0814fd08518805713e4f72e54f2d4d8d1e
Car <matijacarr@gmail.com>
2026-07-08 15:39:56 +0200

    first commit

---
+++ src/main/java/com/springeventspractice/SpringEventsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springeventspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springeventspractice/SpringEventsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springeventspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
